53282ba7c217e928d50507bb10bf9c4cdd327ea9
Brandon <brandondong96@gmail.com>
2024-12-22 17:01:42 -0800

    Add u64 implementations

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,38 @@ pub fn hamming_bitwise_fast(x: &[u8], y: &[u8]) -> u32 {
     distance
 }
 
+#[inline]
+pub fn hamming_bitwise_fast_64(x: &[u8], y: &[u8]) -> u64 {
+    assert_eq!(x.len(), y.len());
+
+    // Process 8 bytes at a time using u64
+    let mut distance = x
+        .chunks_exact(8)
+        .zip(y.chunks_exact(8))
+        .map(|(x_chunk, y_chunk)| {
+            // This is safe because we know the chunks are exactly 8 bytes.
+            // Also, we don't care whether the platform uses little-endian or big-endian
+            // byte order. Since we're only XORing values, we just care that the
+            // endianness is the same for both.
+            let x_val = u64::from_ne_bytes(x_chunk.try_into().unwrap());
+            let y_val = u64::from_ne_bytes(y_chunk.try_into().unwrap());
+            (x_val ^ y_val).count_ones() as u64
+        })
+        .sum::<u64>();
+
+    if x.len() % 8 != 0 {
+        distance += x
+            .chunks_exact(8)
+            .remainder()
+            .iter()
+            .zip(y.chunks_exact(8).remainder())
+            .map(|(x_byte, y_byte)| (x_byte ^ y_byte).count_ones() as u64)
+            .sum::<u64>();
+    }
+
+    distance
+}
+
 #[doc(hidden)]
 #[inline]
 pub fn naive_hamming_distance(x: &[u8], y: &[u8]) -> u64 {
@@ -56,10 +88,29 @@ pub fn naive_hamming_distance(x: &[u8], y: &[u8]) -> u64 {
 
 #[doc(hidden)]
 #[inline]
+pub fn naive_hamming_distance_64(x: &[u8], y: &[u8]) -> u64 {
+    assert_eq!(x.len(), y.len());
+    let mut distance: u64 = 0;
+    for i in 0..x.len() {
+        distance += (x[i] ^ y[i]).count_ones() as u64;
+    }
+    distance
+}
+
+#[doc(hidden)]
+#[inline]
 pub fn naive_hamming_distance_iter(x: &[u8], y: &[u8]) -> u64 {
     x.iter()
         .zip(y)
         .fold(0, |a, (b, c)| a + (*b ^ *c).count_ones()) as u64
+}
+
+#[doc(hidden)]
+#[inline]
+pub fn naive_hamming_distance_iter_64(x: &[u8], y: &[u8]) -> u64 {
+    x.iter()
+        .zip(y)
+        .fold(0, |a, (b, c)| a + (*b ^ *c).count_ones() as u64)
 }
 
 #[test]
@@ -70,12 +121,15 @@ fn all_same_results() {
     let b = hex::decode(b).unwrap();
 
     let expected = naive_hamming_distance(&a, &b);
+    assert_eq!(expected, naive_hamming_distance_64(&a, &b));
 
     // Compare with naive_iter implementation
     assert_eq!(expected, naive_hamming_distance_iter(&a, &b));
+    assert_eq!(expected, naive_hamming_distance_iter_64(&a, &b));
 
     // Compare with auto vectorized implementation
     assert_eq!(expected, hamming_bitwise_fast(&a, &b) as u64);
+    assert_eq!(expected, hamming_bitwise_fast_64(&a, &b));
 
     // Compare with hamming crate
     assert_eq!(expected, hamming::distance_fast(&a, &b).unwrap());
